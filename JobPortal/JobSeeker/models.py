59cfb38c1c37d9de5e71493870809c1cdcfe65cc
sruthy <sruthyjayasankar452@GMAIL.COM>
2021-08-23 19:42:37 +0530

    Added Job Application

--- JobPortal/JobSeeker/models.py
+++ JobPortal/JobSeeker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from Employer.models import Job
 # Create your models here.
+
+
+
+class Application(models.Model):
+    job_title=models.ForeignKey(Job,on_delete=models.CASCADE)
+    candidate_name=models.CharField(max_length=80)
+    age=models.IntegerField()
+    dob=models.CharField(max_length=80)
+    email=models.EmailField(max_length=120)
+    phone_number=models.IntegerField()
+    qualification=models.CharField(max_length=80)
+    college=models.CharField(max_length=120)
+    percent_or_cgpa=models.IntegerField()
+
+    def __str__(self):
+        return self.candidate_name
